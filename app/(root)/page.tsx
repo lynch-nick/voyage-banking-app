71c5a7e5c320054e045cf7df6b3148a9a542f095
NickLynch04 <lynch.nick49@gmail.com>
2024-09-25 15:33:03 -0600

    Implement RightSideBar including Cards Displaying bank info. Implement completed routing using nav bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSideBar from '@/components/RightSideBar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Nick' }
+  const loggedIn = { firstName: 'Nick', lastName: 'Voyage', email: 'contact@gmail.com'}
   return (
     <section className='home'>
       <div className='home-content'>
@@ -20,7 +21,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSideBar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50 }, {currentBalance: 500.50}]}
+      />
     </section>
   )
 }
